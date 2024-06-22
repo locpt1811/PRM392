bde642dffa72992962acfc2c3243ae54d1e9a2f3
locpt1811 <locpt1811@gmail.com>
2024-06-22 10:29:26 +0700

    booklist

---
+++ app/src/main/java/com/example/finalproject/BookDTO.kt
@@ -0,0 +1,14 @@
+package com.example.finalproject
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Book (
+    val book_id: Int,
+    val title: String,
+    val isbn13: String,
+    val language_id: Int,
+    val num_pages: Int,
+    val publication_date: String,
+    val publisher_id: Int
+)
