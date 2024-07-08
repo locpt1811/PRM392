27996a360c727d5665082ff6fe697d238959bbec
stormybringer210 <stormybringer210@gmail.com>
2024-07-08 21:46:44 +0700

    temp fix

--- app/src/main/java/com/example/finalproject/core/alarm/ShoppingAlarmReceiver.kt
+++ app/src/main/java/com/example/finalproject/core/alarm/ShoppingAlarmReceiver.kt
@@ -7,15 +7,15 @@ import com.example.finalproject.core.notification.ShoppingNotifier
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
-@AndroidEntryPoint
-class ShoppingAlarmReceiver : BroadcastReceiver() {
-
-    @Inject
-    lateinit var shoppingNotifier: ShoppingNotifier
-
-    override fun onReceive(context: Context?, intent: Intent?) {
-        context?.let {
-            shoppingNotifier.launchNotification()
-        }
-    }
-}
+//@AndroidEntryPoint
+//class ShoppingAlarmReceiver : BroadcastReceiver() {
+//
+//    @Inject
+//    lateinit var shoppingNotifier: ShoppingNotifier
+//
+//    override fun onReceive(context: Context?, intent: Intent?) {
+//        context?.let {
+//            shoppingNotifier.launchNotification()
+//        }
+//    }
+//}
--- app/src/main/java/com/example/finalproject/core/alarm/ShoppingAlarmScheduler.kt
+++ app/src/main/java/com/example/finalproject/core/alarm/ShoppingAlarmScheduler.kt
@@ -9,37 +9,37 @@ import dagger.hilt.android.qualifiers.ApplicationContext
 import java.util.Calendar
 import javax.inject.Inject
 
-class ShoppingAlarmScheduler @Inject constructor(
-    @ApplicationContext
-    private val context: Context,
-
-    private val alarmManager: AlarmManager
-) : AlarmScheduler {
-
-    override fun createPendingIntent(): PendingIntent {
-        return PendingIntent.getBroadcast(
-            context,
-            0,
-            Intent(context, ShoppingAlarmReceiver::class.java),
-            PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
-        )
-    }
-
-    override fun schedule() {
-        val calendar: Calendar = Calendar.getInstance().apply {
-            timeInMillis = System.currentTimeMillis()
-            set(Calendar.HOUR_OF_DAY, ShoppingNotifierTimeMng.REMINDER_HOUR)
-            set(Calendar.MINUTE, ShoppingNotifierTimeMng.REMINDER_MINUTES)
-        }
-
-        if (calendar.before(Calendar.getInstance())) {
-            calendar.add(Calendar.DATE, 1)
-        }
-
-        alarmManager.set(
-            AlarmManager.RTC_WAKEUP,
-            calendar.timeInMillis,
-            createPendingIntent()
-        )
-    }
-}
+//class ShoppingAlarmScheduler @Inject constructor(
+//    @ApplicationContext
+//    private val context: Context,
+//
+//    private val alarmManager: AlarmManager
+//) : AlarmScheduler {
+//
+//    override fun createPendingIntent(): PendingIntent {
+//        return PendingIntent.getBroadcast(
+//            context,
+//            0,
+//            Intent(context, ShoppingAlarmReceiver::class.java),
+//            PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+//        )
+//    }
+//
+//    override fun schedule() {
+//        val calendar: Calendar = Calendar.getInstance().apply {
+//            timeInMillis = System.currentTimeMillis()
+//            set(Calendar.HOUR_OF_DAY, ShoppingNotifierTimeMng.REMINDER_HOUR)
+//            set(Calendar.MINUTE, ShoppingNotifierTimeMng.REMINDER_MINUTES)
+//        }
+//
+//        if (calendar.before(Calendar.getInstance())) {
+//            calendar.add(Calendar.DATE, 1)
+//        }
+//
+//        alarmManager.set(
+//            AlarmManager.RTC_WAKEUP,
+//            calendar.timeInMillis,
+//            createPendingIntent()
+//        )
+//    }
+//}
